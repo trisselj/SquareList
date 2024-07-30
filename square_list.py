bf969a2efa3b692683e86cc940ee1c0010896d14
trisselj <trisselj@oregonstate.edu>
2024-07-30 16:10:29 -0700

    q

--- square_list.py
+++ square_list.py
@@ -3,6 +3,7 @@
 # Date: 07/30/2024
 # Description: takes as a parameter a list of numbers and replaces each value with the square of that value
 
+# Defines an input method to square a list
 def square_list(numbers):
-    for i in range(len(numbers)):
+    for i in range(len(numbers)): # Modifies the list by squaring without returning a value
         numbers[i] = numbers[i] ** 2
